da4b9a0a5d3c80f0d70ed0fadd8a1393b7d22ce2
tanbre <tannor@breitigam.email>
2021-08-02 08:33:26 -0700

    updated aria selectors to be OS neutral

--- meet_controller.js
+++ meet_controller.js
@@ -2,12 +2,12 @@
 "use strict";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // sender and sendResponse args are used by the browser, do not change the names
+    // `sendResponse` (and maybe `sender`) args are used by the browser, do not change the names
 
-    const turnOffMicButton = document.querySelector('[aria-label="Turn off microphone (⌘ + d)"]')
-    const turnOnMicButton = document.querySelector('[aria-label="Turn on microphone (⌘ + d)"]')
-    const turnOffCamButton = document.querySelector('[aria-label="Turn off camera (⌘ + e)"]')
-    const turnOnCamButton = document.querySelector('[aria-label="Turn on camera (⌘ + e)"]')
+    const turnOffMicButton = document.querySelector('[aria-label*="Turn off microphone"]')
+    const turnOnMicButton = document.querySelector('[aria-label*="Turn on microphone"]')
+    const turnOffCamButton = document.querySelector('[aria-label*="Turn off camera"]')
+    const turnOnCamButton = document.querySelector('[aria-label*="Turn on camera"]')
 
     switch (request.command) {
         case 'toggleMicrophone':
